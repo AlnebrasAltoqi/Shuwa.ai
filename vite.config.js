7e09b1f6b4ad4fba027a4eb412c0ddf9c19b5f8a
AlnebrasAltoqi <alnebras04@gmail.com>
2025-05-23 21:03:43 +0400

    Add PWA manifest and service worker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'], // whatever static files you need
+      manifest: {
+        name: 'Shuwa.AI',
+        short_name: 'Shuwaai',
+        description: 'Delicious recipes—online or offline!',
+        theme_color: '#F97316',
+        background_color: '#FFFBF5',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
+  build: {
+    
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
